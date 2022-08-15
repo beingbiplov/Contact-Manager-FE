a48e5ff9fbc262a7d667924df9a0ab16a99283af
beingbiplov <sharmabiplov@gmail.com>
2022-08-15 12:50:59 +0545

    design navigation bar

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -3,21 +3,25 @@ import { Button, Result } from "antd";
 import React, { useEffect } from "react";
 
 import { verifyToken } from "../../services/userService";
+import Navigation from "./Navigation";
 
 const Dashboard: React.FC = () => {
   useEffect(() => {
     verifyToken();
   }, []);
   return (
-    <Result
-      icon={<SmileOutlined />}
-      title="Work in progress!!"
-      extra={
-        <Button className="primaryBtn" type="primary">
-          Next
-        </Button>
-      }
-    />
+    <div>
+      <Navigation />
+      <Result
+        icon={<SmileOutlined />}
+        title="Work in progress!!"
+        extra={
+          <Button className="primaryBtn" type="primary">
+            Next
+          </Button>
+        }
+      />
+    </div>
   );
 };
 
