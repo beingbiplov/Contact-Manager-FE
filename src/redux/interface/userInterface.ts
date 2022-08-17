e9d000bb08a5713f0518dd25c5af0f5c48cc1dba
beingbiplov <sharmabiplov@gmail.com>
2022-08-17 08:01:07 +0545

    add user data to cookie and implement redux for it

---
+++ src/redux/interface/userInterface.ts
@@ -0,0 +1,5 @@
+export interface userDataInterface {
+  id: number;
+  name: string | undefined;
+  email: string | undefined;
+}
--- src/redux/store/store.tsx
+++ src/redux/store/store.tsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import userAuthReducer from "../slice/userAuthenticationSlice";
+import userDataReducer from "../slice/userDataSlice";
 
 export const store = configureStore({
   reducer: {
     userAuth: userAuthReducer,
+    userData: userDataReducer,
   },
 });
 
